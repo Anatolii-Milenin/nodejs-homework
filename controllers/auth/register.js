540913cb58ff1013d65b0d2c45373b311ad09acf
Anatolii Milenin <anatolymv@gmail.com>
2024-01-17 16:52:44 +0200

    hw-04

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,42 +1,22 @@
 const bcryptjs = require("bcryptjs");
-const { User, schemas } = require("../../models/user");
+const { User } = require("../../models/user");
 const { HttpError } = require("../../helpers");
 
-const register = async (req, res, next) => {
-  try {
-    const { email, password } = req.body;
+const register = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
 
-    const { error } = schemas.registerSchema.validate({ email, password });
-    if (error) {
-      throw HttpError(400, error.details[0].message);
-    }
-
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      throw HttpError(409, "Email in use");
-    }
-
-    const hashPassword = await bcryptjs.hash(password, 10);
-
-    const newUser = await User.create({
-      email,
-      password: hashPassword,
-    });
-
-    res.status(201).json({
-      user: {
-        email: newUser.email,
-        subscription: newUser.subscription,
-      },
-    });
-  } catch (error) {
-    if (error.name === "ValidationError") {
-      error.status = 400;
-    } else if (error.name === "MongoError" && error.code === 11000) {
-      error.status = 409;
-    }
-    next(error);
+  if (user) {
+    throw HttpError(409, "Email in use");
   }
+
+  const hashPassword = await bcryptjs.hash(password, 10);
+  const newUser = await User.create({ ...req.body, password: hashPassword });
+
+  res.status(201).json({
+    name: newUser.name,
+    email: newUser.email,
+  });
 };
 
 module.exports = register;
